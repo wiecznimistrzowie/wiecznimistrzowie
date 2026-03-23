e3c91f492f5fd393946c02b58a9ac66becc0cf9e
Jan Dudulski <jdudulski@gainsight.com>
2026-03-23 19:20:55 +0100

    DCB-compatible event store

--- db/migrations/202603062314_create_event_store_events_table.rb
+++ db/migrations/202603062314_create_event_store_events_table.rb
@@ -5,14 +5,15 @@
     create_table :events do
       primary_key :sequence, type: :Bignum, null: false
 
-      column :event_id, String, null: false
+      column :event_id, :uuid, null: false
       column :event_type, String, null: false
-      column :tags, :jsonb, null: false, default: "{}"
       column :metadata, :jsonb, null: false, default: "{}"
       column :data, :jsonb, null: false
-      column :created_at, :timestamp, null: false
+      column :occurred_at, :timestamp, null: false, default: Sequel.function(:now)
 
       index :event_id, unique: true, name: "index_events_on_event_id"
+      index :event_type, name: "index_events_on_event_type"
+      index :data, opclass: :jsonb_path_ops, name: "index_events_on_data", type: :gin
     end
   end
 
--- infra/db.rb
+++ infra/db.rb
@@ -3,7 +3,7 @@
 module Infra
   DB = Sequel.connect(ENV.delete("DATABASE_URL"))
 
-  DB.extension(:pg_json)
-  Sequel.extension(:pg_json_ops)
+  DB.extension(:pg_json, :pg_array)
+  Sequel.extension(:pg_json_ops, :pg_array_ops)
 end
 
